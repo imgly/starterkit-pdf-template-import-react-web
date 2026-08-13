fa1cea6461df4cf9090947722b05b3396b72762f
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-08-13 12:48:34 +0200

    Update starterkit for v1.80.0

--- src/app/ExampleFileContainer/ExampleFileContainer.tsx
+++ src/app/ExampleFileContainer/ExampleFileContainer.tsx
@@ -1,7 +1,7 @@
 /**
  * ExampleFileContainer - Example PDF files grid
  */
-import { resolveAssetPath } from '../resolveAssetPath';
+import { DEMO_ASSETS_BASE_URL } from '../FileSelection/FileSelection';
 import type { ExampleFile } from '../types';
 import classes from './ExampleFileContainer.module.css';
 
@@ -30,7 +30,7 @@ export function ExampleFileContainer({
               alt={file.alt}
             />
             <img
-              src={resolveAssetPath('/icons/pdf-file.svg')}
+              src={`${DEMO_ASSETS_BASE_URL}/icons/pdf-file.svg`}
               alt="PDF"
               className={classes.fileTypeIcon}
             />
--- src/app/ResultScreen/ResultScreen.tsx
+++ src/app/ResultScreen/ResultScreen.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { CreativeEditor } from '../CreativeEditor/CreativeEditor';
 import { useFileProcessing } from '../FileProcessingContext/FileProcessingContext';
 import { InfoButton } from '../InfoButton/InfoButton';
-import { resolveAssetPath } from '../resolveAssetPath';
+import { DEMO_ASSETS_BASE_URL } from '../FileSelection/FileSelection';
 import classes from './ResultScreen.module.css';
 
 export function ResultScreen() {
@@ -25,7 +25,7 @@ export function ResultScreen() {
   const handleDownloadArchive = () => {
     const link = document.createElement('a');
     link.href = result.sceneArchiveUrl;
-    link.download = `${result.fileName.replace(/\.pdf$/i, '')}.archive.zip`;
+    link.download = `${result.fileName.replace(/\.pdf$/i, '')}.imgly`;
     link.click();
   };
 
@@ -36,7 +36,7 @@ export function ResultScreen() {
           <div className={classes.resultHeader}>
             <button className="btn btn-plain" onClick={() => resetState()}>
               <img
-                src={resolveAssetPath('/icons/chevron-left.svg')}
+                src={`${DEMO_ASSETS_BASE_URL}/icons/chevron-left.svg`}
                 alt="Back"
               />
               <span>New File</span>
@@ -67,7 +67,7 @@ export function ResultScreen() {
               ) : (
                 <div className={classes.noPreview}>
                   <img
-                    src={resolveAssetPath('/icons/pdf-file.svg')}
+                    src={`${DEMO_ASSETS_BASE_URL}/icons/pdf-file.svg`}
                     alt="PDF"
                   />
                   <p>
@@ -99,7 +99,10 @@ export function ResultScreen() {
                 className="btn btn-primary btn-small"
                 onClick={() => setEditorOpen(true)}
               >
-                <img src={resolveAssetPath('/icons/edit.svg')} alt="Edit" />
+                <img
+                  src={`${DEMO_ASSETS_BASE_URL}/icons/edit.svg`}
+                  alt="Edit"
+                />
                 <span>Edit</span>
               </button>
               <button
@@ -107,7 +110,7 @@ export function ResultScreen() {
                 onClick={handleDownloadArchive}
               >
                 <img
-                  src={resolveAssetPath('/icons/download.svg')}
+                  src={`${DEMO_ASSETS_BASE_URL}/icons/download.svg`}
                   alt="Download"
                 />
                 <span>Download CE.SDK Archive</span>
