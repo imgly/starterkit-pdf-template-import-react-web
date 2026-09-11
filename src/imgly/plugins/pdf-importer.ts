2c35df9755c4128379b32022d33666c67296c256
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-11 21:03:56 +0200

    Update starterkit for v1.76.2-rc.1

--- src/imgly/plugins/pdf-importer.ts
+++ src/imgly/plugins/pdf-importer.ts
@@ -21,14 +21,14 @@
  * });
  *
  * // Load into editor
- * await cesdk.load(result.sceneArchiveUrl);
+ * await cesdk.loadFromArchiveURL(result.sceneArchiveUrl);
  *
  * // Clean up when done (at app level)
  * URL.revokeObjectURL(result.imageUrl);
  * URL.revokeObjectURL(result.sceneArchiveUrl);
  * ```
  *
- * @see https://img.ly/docs/cesdk/js/starterkits/pdf-template-import-pdf001/
+ * @see https://img.ly/docs/cesdk/js/features/import-pdf/
  */
 
 import CreativeEngine from '@cesdk/engine';
